3ebe033e7558f1f597abd03ffac6e937ffee028b
thierry.lach <thierry.lach@gmail.com>
2016-02-03 17:22:15 -0500

    Added a comment for the static fields

--- src/org/usfirst/frc/team68/robot/Robot.java
+++ src/org/usfirst/frc/team68/robot/Robot.java
@@ -24,7 +24,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-
+	// Static fields
 	public static AirPump airPump;
 	public static Latches latches;
 	public static Intake intake;
